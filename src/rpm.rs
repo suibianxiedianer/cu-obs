f9d414415992a29b80c9e152587308aa6a1f53e9
Jia Chao <jiac13@chinaunicom.cn>
2023-02-06 14:54:40 +0800

    rpm install 可指定安装目录，可选

--- src/rpm.rs
+++ src/rpm.rs
@@ -1,5 +1,5 @@
 use std::{
-    path::Path,
+    path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
@@ -12,6 +12,7 @@ pub struct RPM;
 
 /// RPM 工具
 impl RPM {
+    /// 判断给定的路径文件是否为 rpm 源码包
     pub fn is_src(file: &Path) -> bool {
         // use file
         let _type = Command::new("file")
@@ -31,6 +32,7 @@ impl RPM {
         _grep.status.success()
     }
 
+    /// 获取软件包的 %{name} 字段
     pub fn get_name(file: &Path) -> crate::Result<String> {
         let _output = Command::new("rpm")
                            .args(["-qp", "--qf", "%{name}", file.to_str().unwrap()])
@@ -44,6 +46,7 @@ impl RPM {
         }
     }
 
+    /// 获取软件包的 %{version} 字段
     pub fn get_version(file: &Path) -> crate::Result<String> {
         let _output = Command::new("rpm")
                            .args(["-qp", "--qf", "%{version}", file.to_str().unwrap()])
@@ -57,6 +60,7 @@ impl RPM {
         }
     }
 
+    /// 获取软件包的 %{release} 字段
     pub fn get_release(file: &Path) -> crate::Result<String> {
         let _output = Command::new("rpm")
                            .args(["-qp", "--qf", "%{release}", file.to_str().unwrap()])
@@ -70,13 +74,24 @@ impl RPM {
         }
     }
 
-    pub fn install_src(file: &Path, workspace: &Workspace, package: &Package) -> crate::Result<()> {
+    /// 接收两个参数：<源码包路径> <Some(安装的路径)>
+    /// 不指定安装目标路径，传 `None` 即可
+    pub fn install_src(file: &Path, target: Option<PathBuf>) -> crate::Result<()> {
+        let mut args = vec![];
+        let mut _specdir = String::new();
+        let mut _sourcedir = String::new();
+
+        if let Some(root) = target {
+            let root = root.to_str().unwrap();
+            _specdir = format!("_spcedir {}", root);
+            _sourcedir = format!("_sourcedir {}", root);
+            args = vec!["--define", &_specdir,
+                        "--define", &_sourcedir];
+        }
+
         let _output = Command::new("rpm")
                            .arg("-i")
-                           .arg("--define")
-                           .arg(format!("_specdir {}", workspace.package_dir(package).to_str().unwrap()))
-                           .arg("--define")
-                           .arg(format!("_sourcedir {}", workspace.package_dir(package).to_str().unwrap()))
+                           .args(&args)
                            .arg(file.to_str().unwrap())
                            .output()
                            .expect("Failed to execute rpm.");
