8ca4609a0d7f8348c6fd49da438bc551b691487c
Jia Chao <jiac13@chinaunicom.cn>
2023-02-08 15:49:48 +0800

    重做 rpm

--- src/rpm.rs
+++ src/rpm.rs
@@ -1,58 +1,101 @@
 use std::{
+    ffi::OsStr,
     path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
+use regex::Regex;
+
 use crate::IsOk;
 
-pub struct RPM;
+pub struct RPM {
+    path: PathBuf,
+    name: String,
+    version: String,
+    release: String,
+}
 
 /// RPM 工具
 impl RPM {
-    /// 判断给定的路径文件是否为 rpm 源码包
-    /// 预留，暂无用处
-    pub fn _is_src(file: &Path) -> crate::Result<()> {
+    /// 新建一个 RPM，并执行初始化检查与字段填充
+    pub fn new<P: ?Sized + AsRef<OsStr>>(path: &P) -> crate::Result<Self> {
+        let path = PathBuf::from(path);
+        let mut rpm = RPM {
+            path,
+            name: String::new(),
+            version: String::new(),
+            release: String::new(),
+        };
+
+        rpm.init()
+    }
+
+    /// 检查 rpm 的合法性，填充 name, version, release 信息
+    fn init(mut self) -> crate::Result<Self> {
+        if !self._is_rpm() {
+            return Err(
+                format!("{} is not a regular rpm file!", self.path.to_str().unwrap()).into(),
+            );
+        }
+        self.name = self.get_name().unwrap();
+        self.version = self.get_version().unwrap();
+        self.release = self.get_release().unwrap();
+
+        Ok(self)
+    }
+
+    /// 判断给定的路径文件是否为 rpm 格式
+    pub fn is_rpm<P: ?Sized + AsRef<OsStr>>(path: &P) -> bool {
         // use file
-        let _type = Command::new("file")
-            .arg(file.to_str().unwrap())
-            .stdout(Stdio::piped())
-            .spawn()
+        let _output = Command::new("file")
+            .arg(path)
+            .output()
             .expect("Failed to execute file.");
 
-        // grep key words
-        let _output = Command::new("grep")
-            .arg("RPM v.* src$")
-            .stdin(Stdio::from(_type.stdout.unwrap()))
-            .output()
-            .expect("failed to execute grep");
+        let re = Regex::new(r"RPM v.*").unwrap();
+        re.is_match(String::from_utf8(_output.stdout).unwrap().as_ref())
+    }
 
-        _output.is_ok()
+    /// inner: is_rpm
+    fn _is_rpm(&self) -> bool {
+        Self::is_rpm(&self.path)
+    }
+
+    /// 判断给定的路径文件是否为 rpm 源码包
+    pub fn is_src<P: ?Sized + AsRef<OsStr>>(path: &P) -> bool {
+        let _output = Command::new("file")
+            .arg(path)
+            .output()
+            .expect("Failed to execute file.");
+
+        let re = Regex::new(r"RPM v.* src\n").unwrap();
+        re.is_match(String::from_utf8(_output.stdout).unwrap().as_ref())
+    }
+
+    /// inner: is_src
+    fn _is_src(&self) -> bool {
+        Self::is_src(&self.path)
     }
 
     /// 获取软件包的 %{name} 字段
-    pub fn get_name(file: &Path) -> crate::Result<String> {
-        let _output = Command::new("rpm")
-            .args(["-qp", "--qf", "%{name}", file.to_str().unwrap()])
-            .output()
-            .expect("Failed to excute rpm.");
-
-        _output.result()
+    pub fn get_name(&self) -> crate::Result<String> {
+        self.query_format("%{name}")
     }
 
     /// 获取软件包的 %{version} 字段
-    pub fn get_version(file: &Path) -> crate::Result<String> {
-        let _output = Command::new("rpm")
-            .args(["-qp", "--qf", "%{version}", file.to_str().unwrap()])
-            .output()
-            .expect("Failed to excute rpm.");
-
-        _output.result()
+    pub fn get_version(&self) -> crate::Result<String> {
+        self.query_format("%{version}")
     }
 
     /// 获取软件包的 %{release} 字段
-    pub fn get_release(file: &Path) -> crate::Result<String> {
+    pub fn get_release(&self) -> crate::Result<String> {
+        self.query_format("%{release}")
+    }
+
+    /// 查询并格式化
+    fn query_format(&self, qf: &str) -> crate::Result<String> {
         let _output = Command::new("rpm")
-            .args(["-qp", "--qf", "%{release}", file.to_str().unwrap()])
+            .args(["-qp", "--qf", qf])
             .output()
             .expect("Failed to excute rpm.");
 
@@ -61,7 +104,7 @@ impl RPM {
 
     /// 接收两个参数：<源码包路径> <Some(安装的路径)>
     /// 不指定安装目标路径，传 `None` 即可
-    pub fn install_src(file: &Path, target: Option<PathBuf>) -> crate::Result<()> {
+    pub fn install_src(&self, target: Option<PathBuf>) -> crate::Result<()> {
         let mut args = vec![];
         let mut _specdir = String::new();
         let mut _sourcedir = String::new();
@@ -76,7 +119,7 @@ impl RPM {
         let _output = Command::new("rpm")
             .arg("-i")
             .args(&args)
-            .arg(file.to_str().unwrap())
+            .arg(&self.path.to_str().unwrap())
             .output()
             .expect("Failed to execute rpm.");
 
