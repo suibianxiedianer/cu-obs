de6354f257ac973c9ccbd3685dd72082bf5a953a
Jia Chao <jiac13@chinaunicom.cn>
2023-02-08 17:12:52 +0800

    workspace: test done

--- src/workspace.rs
+++ src/workspace.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::fs;
 use std::path::{Path, PathBuf};
 
@@ -14,12 +15,15 @@ pub struct Workspace {
 impl Workspace {
     // init workspace, clean tempdir?
     // TODO: not confirm yet
-    pub fn init() -> crate::Result<Self> {
-        let root = PathBuf::from(WORKSPACE).canonicalize().unwrap();
-        let temp = root.join("_tmp");
+    pub fn init<P: ?Sized + AsRef<OsStr>>(path: &P) -> crate::Result<Self> {
+        let mut root = PathBuf::from(path);
+        let mut temp = root.join("_tmp");
         fs::create_dir_all(&temp)?;
 
-        Ok(Workspace { root, temp })
+        Ok(Workspace {
+            root: root.canonicalize().unwrap(),
+            temp: temp.canonicalize().unwrap(),
+        })
     }
 
     /// root 字段
@@ -70,8 +74,44 @@ impl Workspace {
         if path.is_file() {
             fs::remove_file(path).map_err(|e| e.into())
         } else {
-            fs::remove_dir_all(&path)?;
-            fs::remove_dir(path).map_err(|e| e.into())
+            fs::remove_dir_all(&path).map_err(|e| e.into())
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+
+    use super::{Package, Workspace};
+
+    #[test]
+    fn base() {
+        let ws = Workspace::init("./workspace").unwrap();
+
+        let _tmp = ws.temp();
+
+        let pkg = Package::new("prj", "pkg");
+
+        let pkg_dir = ws.package_dir(&pkg);
+        let mut _osc = pkg_dir.clone();
+        _osc.push(".osc");
+
+        fs::create_dir_all(&_osc.to_str().unwrap());
+
+        use std::fs::File;
+        let mut _demo = pkg_dir.clone();
+        _demo.push("demo");
+        {
+            File::create(&_demo).unwrap();
+        }
+
+        ws.clean_source(&pkg);
+        assert!(!_demo.exists());
+        assert!(_osc.exists());
+
+        ws.remove_package(&pkg).unwrap();
+        ws.remove_project(&pkg).unwrap();
+        Workspace::remove(&ws.root);
     }
 }
